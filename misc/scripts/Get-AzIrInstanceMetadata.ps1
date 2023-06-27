5ea9cef9aa6647c022d1d7469b40e0e1e6f8bfbb
Karim El-Melhaoui <26272119+karimelmel@users.noreply.github.com>
2023-06-27 16:30:16 +0200

    Create Get-AzIrInstanceMetadata.ps1

---
+++ misc/scripts/Get-AzIrInstanceMetadata.ps1
@@ -0,0 +1,16 @@
+function Get-AzIrInstanceMetadata  {
+    [CmdLetBinding()]
+    param ( 
+        [Parameter(Mandatory = $false)]
+        [string]
+        $FileName = "AzIrInstanceMetadata",
+        [Parameter(Mandatory = $false)]
+        [string]
+        $FilePath = ( $env:TEMP )
+
+    )
+    if(-not($PSBoundParameters.ContainsKey('FileName')) -and $FileName) {
+        $FileName = (Join-Path $FilePath -ChildPath $FileName".json" )
+    }
+    Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | ConvertTo-Json -Depth 64 | Out-File -FilePath $FileName
+}
